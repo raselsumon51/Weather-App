614a956057eebaccd3db8ee1d2fc7470cba8be06
Rasel Sumon <raselsumon51@gmail.com>
2023-12-07 15:21:00 +0600

    fetch done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
 
 function App() {
+  const apiKey = 'b52d59311b1bebf2d687f5356604ea89';
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key == "Enter") {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data)
+          // console.log(data)
+          // // setCity("")
+        )
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Weather Details</h1>
+      <input
+        className="input"
+        placeholder="Enter City Name"
+        onChange={e => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
+      />
+
+      {typeof weatherData.main === 'undefined' ? (
+        <div className="">
+          <p className="hints">Welcome to weather app! Enter in a city to get the weather of.</p>
+        </div >
+      ) : (
+        <div className="weather-data">
+          <p className="city">{weatherData.name}</p>
+          <p className="temp">{weatherData.main.temp} F</p>
+          <p className="weather">{weatherData.weather[0].main}</p>
+        </div>
+      )
+      }
+
     </div>
   );
 }
